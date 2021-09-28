94232a593f2b69fdf8efa84d7fe3bba049d5ee3b
Ben Clarke <benthedev@outlook.com>
2021-09-28 23:03:35 +0100

    Forgot myself and added all the required pages. Links work and some basic faq content added

--- home.rb
+++ home.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get "/" do
     erb :index
 end
+
+get "/store" do
+    erb :store
+end
+
+get "/gallery" do
+    erb :gallery
+end
+
+get "/faq" do
+    erb :faq
+end
+
+get "/about" do
+    erb :about
+end
