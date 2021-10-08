1ffa0d4d03fd1ece6d6e1fdad5a5d76155fab962
Ben Clarke <benthedev@outlook.com>
2021-10-08 01:27:10 +0100

    Added stipe checkout to site with basic functionality

--- home.rb
+++ home.rb
@@ -1,3 +1,4 @@
+require 'stripe'
 require 'sinatra'
 
 get "/" do
@@ -18,4 +19,63 @@ end
 
 get "/about" do
     erb :about
+end
+
+get "/success" do
+    erb :success
+end
+
+get "/cancel" do
+    erb :cancel
+end
+
+# This is your real test secret API key.
+Stripe.api_key = 'sk_test_51HXRUPJQBIZrH8yyQLsDsjFYctrNHRftaBIeEs4UQ6rnM5OY0jRTX5AaYSPlFEmAM5tI1ag3NFiHFCkqFfhwSNm800IbjU851m'
+set :static, true
+set :port, 4242
+
+YOUR_DOMAIN = 'http://127.0.0.1:4242'
+
+post '/create-checkout-session-white' do
+    content_type 'application/json'
+    session = Stripe::Checkout::Session.create({
+      line_items: [{
+        # TODO: replace this with the `price` of the product you want to sell
+        price: 'price_1Ji6QvJQBIZrH8yyJaNdcwdv',
+        quantity: 1,
+      }],
+      shipping_rates: ["shr_1Ji6ZPJQBIZrH8yy6UABTSvF"],
+      shipping_address_collection: {
+        allowed_countries: ['GB'],
+      },
+      payment_method_types: [
+        'card',
+      ],
+      mode: 'payment',
+      success_url: YOUR_DOMAIN + '/success',
+      cancel_url: YOUR_DOMAIN + '/cancel',
+    })
+    redirect session.url, 303
+  end
+
+post '/create-checkout-session-charcoal' do
+  content_type 'application/json'
+  session = Stripe::Checkout::Session.create({
+    line_items: [{
+      # TODO: replace this with the `price` of the product you want to sell
+      price: 'price_1Ji6XIJQBIZrH8yyWiu2ObD0',
+      quantity: 2,
+    }],
+    shipping_rates: ["shr_1Ji6ZPJQBIZrH8yy6UABTSvF"],
+    shipping_address_collection: {
+      allowed_countries: ['GB'],
+    },
+    payment_method_types: [
+      'card',
+    ],
+    mode: 'payment',
+    success_url: YOUR_DOMAIN + '/success',
+    cancel_url: YOUR_DOMAIN + '/cancel',
+  })
+  redirect session.url, 303
 end
